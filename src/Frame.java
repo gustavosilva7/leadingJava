afea15776f8878342e157620312a8962c11aa13f
gustavosilva7 <gustavosilvaalexgusta007@gmail.com>
2024-05-24 07:58:27 -0300

    chore: Update button text and size in Frame class

--- src/Frame.java
+++ src/Frame.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.plaf.FontUIResource;
 
 public class Frame {
@@ -14,11 +17,20 @@ public class Frame {
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
 
-        JButton button = new JButton("Teste");
-        button.setBounds(200, 200, 160, 60);
+        JButton button = new JButton("java é melhor linguagem do mundo!!");
+        button.setBounds(100, 200, 460, 60);
         button.setFont(new FontUIResource("Arial", Font.BOLD, 20));
         button.setForeground(new Color(240, 185, 235));
         button.setBackground(new Color(15, 15, 15));
+        button.addActionListener(new teste());
         frame.add(button);
+
+
     }
+    public class teste implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "java é melhor linguagem do mundo!!");
+        }
+    }
+
 }
